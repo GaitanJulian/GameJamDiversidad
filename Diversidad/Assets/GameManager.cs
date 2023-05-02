4829417032b7e3d765e4aef4ddbae3350c833170
GaitanJulian <juliangaitan_h@hotmail.com>
2023-05-01 21:47:59 -0500

    Delete debug log in console

--- Diversidad/Assets/GameManager.cs
+++ Diversidad/Assets/GameManager.cs
@@ -11,14 +11,12 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         puntosTotales = 0;
-        Debug.Log("puntosTotales initialized to 0.");
     }
 
 
     public void SumarPuntos(int puntosASumar)
     {
         puntosTotales += puntosASumar;
-        Debug.Log("Adding " + puntosASumar + " points. Total points: " + puntosTotales);
     }
 
 
