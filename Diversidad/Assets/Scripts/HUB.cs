09fd4d7ed87785c49a9088b10978d479d0ad8beb
GaitanJulian <juliangaitan_h@hotmail.com>
2023-05-02 20:17:01 -0500

    UI when player dies

--- Diversidad/Assets/Scripts/HUB.cs
+++ Diversidad/Assets/Scripts/HUB.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class HUB : MonoBehaviour
 { 
+
     [SerializeField] private TextMeshProUGUI puntos;
+    [SerializeField] private GameObject panel;
 
     // Start is called before the first frame 
     private void Awake()
     {
         GameManager.OnPointsUpdate += UpdatePointsText;
+        startUI();
     }
 
+    private void Start()
+    {
+        PlayerController.OnPlayerDead += loseUI;
+    }
     private void UpdatePointsText()
     {
         puntos.text = "Points: " + GameManager.Instance.PuntosTotales;
@@ -22,6 +30,22 @@ public class HUB : MonoBehaviour
     private void OnDestroy()
     {
         GameManager.OnPointsUpdate -= UpdatePointsText;
+        PlayerController.OnPlayerDead -= loseUI;
+
+
+    }
+
+    private void startUI()
+    {
+        puntos.gameObject.SetActive(true);
+        panel.gameObject.SetActive(false);
+    }
+
+    private void loseUI()
+    {
+        puntos.gameObject.SetActive(false);
+        panel.gameObject.SetActive(true);
+
     }
 
 }
